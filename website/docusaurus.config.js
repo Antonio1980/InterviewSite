bb1c894595bc9a0a13d010509a84f2a917f2f264
Antonio1980 <antishipul@gmail.com>
2021-06-05 16:37:01 +0300

    fixes

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,63 +1,61 @@
 module.exports = {
-  title: 'Tech Interview Handbook',
-  tagline:
-    'Carefully curated content to help you ace your next technical interview',
-  url: 'https://techinterviewhandbook.org',
-  baseUrl: '/',
-  favicon: 'img/favicon.png',
-  organizationName: 'yangshun',
-  projectName: 'tech-interview-handbook',
-  themeConfig: {
-    prism: {
-      theme: require('prism-react-renderer/themes/github'),
-      darkTheme: require('prism-react-renderer/themes/dracula'),
-    },
-    navbar: {
-      title: 'Tech Interview Handbook',
-      logo: {
-        alt: 'Tech Interview Handbook Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {to: 'introduction', label: 'Getting Started', position: 'right'},
-        {to: 'blog', label: 'Blog', position: 'right'},
-        {
-          href: 'https://github.com/yangshun/tech-interview-handbook',
-          label: 'GitHub',
-          position: 'right',
+    title: 'Tech Interview Handbook',
+    tagline: 'Carefully curated content to help you ace your next technical interview',
+    url: 'https://techinterviewhandbook.org',
+    baseUrl: '/',
+    favicon: 'img/favicon.png',
+    organizationName: 'yangshun',
+    projectName: 'tech-interview-handbook',
+    themeConfig: {
+        prism: {
+            theme: require('prism-react-renderer/themes/github'),
+            darkTheme: require('prism-react-renderer/themes/dracula'),
         },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} Yangshun Tay. Built with Docusaurus.`,
-    },
-    gtag: {
-      trackingID: 'UA-44622716-2',
-    },
-    algolia: {
-      apiKey: 'bd359779d1c4c71ade6062e8f13f5a83',
-      indexName: 'yangshun-tech-interview',
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          path: '../contents',
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/yangshun/tech-interview-handbook/edit/master/contents/',
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
+        navbar: {
+            title: 'Tech Interview Handbook',
+            logo: {
+                alt: 'Tech Interview Handbook Logo',
+                src: 'img/logo.svg',
+            },
+            items: [
+                { to: 'introduction', label: 'Getting Started', position: 'right' },
+                { to: 'blog', label: 'Blog', position: 'right' },
+                {
+                    href: 'https://antonio1980.github.io/InterviewSite/',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
         },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+        footer: {
+            style: 'dark',
+            copyright: `Copyright © ${new Date().getFullYear()} Yangshun Tay. Built with Docusaurus.`,
         },
-      },
+        gtag: {
+            trackingID: 'UA-44622716-2',
+        },
+        algolia: {
+            apiKey: 'bd359779d1c4c71ade6062e8f13f5a83',
+            indexName: 'yangshun-tech-interview',
+        },
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    path: '../contents',
+                    routeBasePath: '/',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    editUrl: 'https://github.com/Antonio1980/InterviewSite/edit/master/contents',
+                    showLastUpdateAuthor: true,
+                    showLastUpdateTime: true,
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
     ],
-  ],
-  clientModules: [require.resolve('./src/components/SidebarAd')],
+    clientModules: [require.resolve('./src/components/SidebarAd')],
 };
